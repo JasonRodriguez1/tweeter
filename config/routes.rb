669f6c4f4b9e245d7fb3b732e9bc2da067200502
Robert Grayson <bobbygrayson@gmail.com>
2013-11-04 18:31:27 -0500

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TwitterClone::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
+
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Tweeter'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Tweeter')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Tweeter | Home")
+    end
+  end 
+
+  describe "Help page" do
+
+	it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+	end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Tweeter | Help")
+    end
+  end
+  
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Tweeter | About Us")
+    end
+  end
+end
