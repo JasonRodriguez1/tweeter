1d936e7fb8ebf7d3e79eb210279e89cc71d88ee8
Jason <jrodriguez.actual@gmail.com>
2013-11-06 12:29:30 -0800

    completed user signup

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
   gem 'growl', '1.0.3'
+  gem 'factory_girl_rails', '4.2.1'
 end
 
 gem 'sass-rails', '4.0.1'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "Welcome to the Tweeter App!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  private
+
+	  def user_params
+	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	  end
 end
