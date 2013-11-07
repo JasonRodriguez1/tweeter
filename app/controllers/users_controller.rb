9cc165be1098094f27b7fc360ba81b31cd365e13
Jason <jrodriguez.actual@gmail.com>
2013-11-06 21:38:52 -0800

    completed sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params)
-  	if @user.save
-  		flash[:success] = "Welcome to the Tweeter App!"
-  		redirect_to @user
-  	else
-  		render 'new'
-  	end
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Tweeter App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def new
